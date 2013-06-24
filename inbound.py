ebd68a4527135e485437c105ca19aa4c1ce6a0c6
Hsiu-Fan Wang <hfwang@porkbuns.net>
2013-06-24 10:12:34 +0100

    Upgrade to python2.7

--- inbound.py
+++ inbound.py
@@ -1,5 +1,4 @@
-import logging, email, yaml
-from django.utils import simplejson as json
+import logging, email, yaml, json
 from google.appengine.ext import webapp, deferred
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api.urlfetch import fetch
@@ -9,10 +8,10 @@ settings = yaml.load(open('settings.yaml'))
 
 def callback(raw):
   result = {'email': {'raw': raw}}
-  
-  response = fetch(settings['outbound_url'], 
-              payload=json.dumps(result), 
-              method="POST", 
+
+  response = fetch(settings['outbound_url'],
+              payload=json.dumps(result),
+              method="POST",
               headers={
                 'Authorization': settings['api_key'],
                 'Content-Type': 'application/json'

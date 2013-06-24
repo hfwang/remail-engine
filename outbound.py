860d3c7d681b5745af814c4720e8d5e5e01a1edc
Hsiu-Fan Wang <hfwang@porkbuns.net>
2013-06-24 10:18:23 +0100

    Send email via mandrill
    Mandrill has a 12k/month free quota, which is super nice. Unfortunately
    appengine doesn't give you access to a way to check how many more free emails
    you can send hence the switch

--- outbound.py
+++ outbound.py
@@ -1,17 +1,70 @@
 import logging, yaml, json
+from email.utils import parseaddr
 from google.appengine.ext import webapp, deferred
-from google.appengine.api import mail
+from google.appengine.api import mail, urlfetch
 
 settings = yaml.load(open('settings.yaml'))
 
 def email(body):
   email = json.loads(body)
+
   # For some reason, ActiveResource is very adamant about generating a JSON
   # root... so unnest it if needed
   if 'email' in email:
     email = email['email']
 
-  mail.EmailMessage(**email).send()
+  if settings['mandrill_api_key'] and settings['mandrill_api_key'] != 'changeme':
+    send_via_mandrill(email)
+  else:
+    send_via_appengine(email)
+
+def send_via_mandrill(args):
+  mandrill_url = 'https://mandrillapp.com/api/1.0/messages/send.json'
+  payload = { 'to': [] }
+
+  # Translate fields from -> to
+  fields = [
+    ('subject', 'subject'),
+    ('html', 'html'),
+    ('body', 'text'),
+    ('headers', 'headers'),
+  ]
+  for src, dest in fields:
+    if src in args:
+      payload[dest] = args[src]
+
+  from_name, from_addr = parseaddr(args['sender'])
+  payload['from_email'] = from_addr
+  if from_name:
+    payload['from_name'] = from_name
+
+  if isinstance(args['to'], basestring):
+    args['to'] = [args['to']]
+  for recipient in args['to']:
+    name, addr = parseaddr(recipient)
+    struct = { 'email': addr }
+    if name:
+      struct['name'] = name
+    payload['to'].append(struct)
+
+  payload = {
+    "key": settings['mandrill_api_key'],
+    "message": payload
+  }
+
+  logging.debug("Sending via mandrill: %s", json.dumps(payload))
+  content = urlfetch.fetch(
+    mandrill_url,
+    method=urlfetch.POST,
+    headers={'Content-Type': 'application/json'},
+    payload=json.dumps(payload))
+  if content.status_code == 200:
+    logging.info("Successfully sent")
+  else:
+    logging.error("Failed to send: %s", content)
+
+def send_via_appengine(args):
+  mail.EmailMessage(**args).send()
 
 class OutboundHandler(webapp.RequestHandler):
   def post(self, *args):

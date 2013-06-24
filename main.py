27f8ab50b34a90751d337550e0fa3b2b76ef0af6
Hsiu-Fan Wang <hfwang@porkbuns.net>
2013-06-24 10:12:34 +0100

    Upgrade to python2.7

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ from google.appengine.ext.webapp import util
 from outbound import OutboundHandler
 from inbound import InboundHandler
 
-def main():
-  application = webapp.WSGIApplication([
-                                         ('/emails(\.json)*', OutboundHandler), 
-                                         InboundHandler.mapping()
-                                       ],
-                                       debug=True)
-  util.run_wsgi_app(application)
-
-
-if __name__ == '__main__':
-  main()
+app = webapp.WSGIApplication([
+    ('/emails(\.json)*', OutboundHandler),
+    InboundHandler.mapping()
+  ],
+  debug=True)
--- outbound.py
+++ outbound.py
@@ -1,34 +1,20 @@
-import logging, yaml
-from django.utils import simplejson as json
+import logging, yaml, json
 from google.appengine.ext import webapp, deferred
 from google.appengine.api import mail
 
 settings = yaml.load(open('settings.yaml'))
 
-def safe_dict(d): 
-  """
-    Recursively clone json structure with UTF-8 dictionary keys
-    http://bugs.python.org/issue2646
-  """ 
-  if isinstance(d, dict): 
-    return dict([(k.encode('utf-8'), safe_dict(v)) for k,v in d.iteritems()]) 
-  elif isinstance(d, list): 
-    return [safe_dict(x) for x in d] 
-  else: 
-    return d
-    
 def email(body):
   email = json.loads(body)
-  mail.EmailMessage(**safe_dict(email)).send()
+  mail.EmailMessage(**email).send()
 
 class OutboundHandler(webapp.RequestHandler):
-
   def post(self, *args):
     api_key = self.request.headers.get('Authorization')
-    
+
     if api_key != settings['api_key']:
       logging.error("Invalid API key: " + str(api_key))
       self.error(401)
       return
-    
+
     deferred.defer(email, self.request.body, _queue='outbound')
